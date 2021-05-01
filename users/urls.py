e0933ad8413052ed134d5bff87f6bc736e5c6cfc
costalferz <costalferzus@gmail.com>
2021-05-02 01:43:07 +0700

    Login

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls.static import static
 from django.urls import path
 from . import views
 #from django.contrib.auth import views as auth_views
-from .views import SignUpView
+from .views import SignUpView,Loginform
 #from .views import 
 urlpatterns = [
     path('Register', views.SignUpView.as_view(), name='Register'),
+    path('Login', views.Loginform, name='Login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render,get_object_or_404
-from django.urls import reverse_lazy
-from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render,get_object_or_404
+from django.urls import reverse_lazy,reverse
+from django.http import HttpResponseRedirect, HttpResponse
+
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -14,4 +16,37 @@ class SignUpView(CreateView):
     form_class = UserRegisterForm
     success_url = reverse_lazy('login')
     template_name = 'Signup.html'
+
+def Loginform(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                return HttpResponse("Your account was inactive.")
+        else:
+            print("Someone tried to login and failed.")
+            print("They used username: {} and password: {}".format(username, password))
+            return HttpResponse("Invalid login details given")
+    else:
+        return render(request, 'Login.html')
+
+
+# def Loginform(request):
+#     username = request.POST['username']
+#     password = request.POST['password']
+#     #check username, password
+#     user = authenticate(username=username,password=password)
+    
+#     if user is not None:
+#         auth.login(request,user)
+#         return redirect('home')
+#     else:
+#         messages.info(request,"Doesn't Match Any Username or Password")
+#         return redirect('Login.html')
+
 
