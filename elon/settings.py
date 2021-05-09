a444936b6920778ac3b97b87dd65d0f0606abe5d
costalferz <costalferzus@gmail.com>
2021-05-10 05:00:32 +0700

    git push

--- elon/settings.py
+++ elon/settings.py
@@ -128,7 +128,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
