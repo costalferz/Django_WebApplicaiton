997683b8bc5ba0cfaf0d84d624b95c6f3e53bc76
costalferz <costalferzus@gmail.com>
2021-05-10 05:23:09 +0700

    push

--- elon/urls.py
+++ elon/urls.py
@@ -21,4 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('item.urls')),
     path('', include('users.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
